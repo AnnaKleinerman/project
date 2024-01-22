94242d3eb083a8dd0507507d8c48ab9f5cc938f1
AnnaKleinerman <anna.kleinerman@gmail.com>
2024-01-22 23:29:27 +0200

    modified:   app.py

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ price_range = st.slider(
 
 actual_range=list(range(price_range[0],price_range[1]+1))
 
-transmission = st.checkbox('automatic')
+transmission = st.checkbox('manual')
 
 if transmission:
     filtered_data=data[data.price.isin(actual_range)]
-    filtered_data=filtered_data[data.transmission=='automatic']
+    filtered_data=filtered_data[data.transmission=='manual']
 else:
     filtered_data=data[data.price.isin(actual_range)]
 
@@ -51,7 +51,10 @@ st.plotly_chart(fig2)
 
 st.write('Here are your options with a split by Transmission ans days listed')
 
-fig = px.histogram(filtered_data, x=':red"transmission"', y=':green"days_listed"')           
+fig = px.histogram(filtered_data, x="transmission", y="days_listed")           
+st.plotly_chart(fig)
+
+fig = px.line_3d(filtered_data, x="price", y="condition")           
 st.plotly_chart(fig)
 
 st.write('Here is the list of latest date_posted ad')
